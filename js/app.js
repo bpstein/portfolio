904c9fca4d1f8cf0315d551db9b4abe1809add69
Ben Stein <bumstain@gmail.com>
2016-02-14 19:47:18 +0000

    Added projects

--- js/app.js
+++ js/app.js
@@ -3,11 +3,20 @@ $(window).load( function() {
     $('#container').masonry({
         "itemSelector": ".item",
         "columnWidth": ".grid-sizer",
+        cornerStampSelector: '.corner-stamp'
     });
 
 });
 
 $(document).ready(function() {
+
+	$('.item').on('click', function popup() {
+		$('#popup').fadeIn(450);
+	});
+
+	$('#popup').on('click', function popout(){
+		$('#popup').fadeOut(450);
+	})
 
 });
 
