0f045142d6caa5022a6fc05ca7d5524e784fef78
Ben Stein <bumstain@gmail.com>
2016-02-28 22:37:50 +0000

    Added navbar filter funtbar filter functionality

--- js/app.js
+++ js/app.js
@@ -1,30 +1,85 @@
-$(window).load( function() {
+$(window).load(function() {
 
-    $('#container').masonry({
-        "itemSelector": ".item",
-        "columnWidth": ".grid-sizer",
-        cornerStampSelector: '.corner-stamp'
-    });
+	// MASONRY GRID FUNCTIONALITY // 
+  $('#container').masonry({
+      "itemSelector": ".item",
+      "columnWidth": ".grid-sizer",
+  });
 
 });
 
 $(document).ready(function() {
 
+	// DECLARE HTML ELEMENTS FOR PROJECT GRID
+	var HTMLitem = '<div class="item frontend"></div>';
+	var HTMLitemImage = '<img src="%data%" class="image">';
+	var HTMLitemAnchor = '<a class="overlay" href="#" target="_blank"</a>';
+	var HTMLitemDescription = '<div class="description"></div>';
+	var HTMLitemTitle = '<h3 class="title">%data%</h3>';
+	var HTMLitemOverview = '<div class="overview"><p>%data%</p></div>';
+
+	// APPEND HTML ELEMENTS TO PROJECT GRID
+	projects.display = function() {
+		for (project in projects.projects) {
+			var formattedItemImage = HTMLitemImage.replace("%data%", projects.projects[project].image);
+			var formattedItemDescription = HTMLitemDescription;
+			var formattedItemAnchor = HTMLitemAnchor.replace("%data%", projects.projects[project].url);
+			var formattedTitle = HTMLitemTitle.replace("%data%", projects.projects[project].name);
+			var formattedOverview = HTMLitemOverview.replace("%data%", projects.projects[project].overview);
+			$("#container").append(HTMLitem); // Append project item to grid
+			$(".item").append(formattedItemImage); // Append image to item
+			$(".item").append(formattedItemDescription); // Append description div to item
+			$(".description").append(formattedTitle).append(formattedOverview); 
+			// $(".title").append(formattedItemAnchor); // Append url to title
+		}
+	}
+
+	// UNCOMMENT THE BELOW FUNCTION CALL TO RUN JSON OBJECTS
+	// projects.display(); 
+
+	// NAVBAR FILTER 
+	$('nav a').on('click', function() {
+
+		var filter = $(this).text().toLowerCase().replace(' ', '-');
+		console.log(filter);
+
+		$('#container .item').each(function() {
+			if (!$(this).hasClass(filter)) {
+				$(this).hide().addClass('.hide').removeClass('.show');
+			}
+
+			else if ($(this).hasClass(filter)) {
+				$(this).show().addClass('.show');
+			}
+
+			else {
+				$(this).removeClass('.hide').addClass('.show');
+			}
+		});
+
+		return false;
+
+	});
+
+	// Clear all filter classes when ALL button is clicked
+	$('.all').on('click', function() {
+		$('div').removeClass('.hide').addClass('.show');
+	});
+
+	// MODAL WINDOW FUNCTIONALITY //
 	$('.item').on('click', function popup() {
 		$('#popup').fadeIn(450);
 	});
 
-	var appendthis =  ("<div class='modal-overlay js-modal-close'></div>");
+	var appendModal =  ("<div class='modal-overlay js-modal-close'></div>");
 
 	  $('a[data-modal-id]').click(function(e) {
 	    e.preventDefault();
-	    $("body").append(appendthis);
+	    $("body").append(appendModal);
 	    $(".modal-overlay").fadeTo(500, 0.7);
-	    //$(".js-modalbox").fadeIn(500);
 	    var modalBox = $(this).attr('data-modal-id');
 	    $('#'+modalBox).fadeIn($(this).data());
 	  });  
-	  
 	  
 	$(".js-modal-close, .modal-overlay").click(function() {
 	  $(".modal-box, .modal-overlay").fadeOut(500, function() {
---
+++ js/projectData.js
@@ -0,0 +1,34 @@
+var projects = {
+	"projects": [{
+			"name": "Street Fighter",
+			"overview": "Street Fighter Ryu with basic jQuery effects",
+			"image": "img/projects/streetfighter.jpg",
+			"url": "http://www.bumstain.github.io/jquery-streetfighter"
+		}, {
+			"name": "Tic Tac Toe",
+			"overview": "JavaScript TicTacToe",
+			"image": "img/projects/tictactoe.png",
+			"url": "http://www.bumstain.github.io/javascript-tictactoe"
+		}, {
+			"name": "SearchVidz YouTube API",
+			"overview": "Video search site using the YouTube API",
+			"image": "img/projects/youtube.jpg",
+			"url": "http://www.bumstain.github.io/javascript-tictactoe"
+		}, {
+			"name": "Marvel Quiz Project",
+			"overview": "Basic JavaScript quiz app based on weird facts from the Marvel universe",
+			"image": "img/projects/marvelquiz.png",
+			"url": "http://www.bumstain.github.io/marvel-quiz"
+		}, {
+			"name": "Content Slider",
+			"overview": "Simple jQuery content slider",
+			"image": "img/projects/contentslider.png",
+			"url": "http://www.bumstain.github.io/content-slider"
+		}, {
+			"name": "FAQ Slider",
+			"overview": "Accordion slider that uses jQuery, similar to FAQ menus",
+			"image": "img/projects/FAQslider.png",
+			"url": "http://www.bumstain.github.io/FAQ-slider"
+		}
+	]
+};
