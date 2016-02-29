bc4a78fa6f968e07824ed1a4d35542742f241e4a
Ben Stein <bumstain@gmail.com>
2016-02-29 23:27:38 +0000

    Dynamically feed grid items and classes from JSON objects

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 $(window).load(function() {
 
-	// MASONRY GRID FUNCTIONALITY // 
+  // MASONRY GRID FUNCTIONALITY // 
   $('#container').masonry({
       "itemSelector": ".item",
       "columnWidth": ".grid-sizer",
@@ -10,90 +10,92 @@ $(window).load(function() {
 
 $(document).ready(function() {
 
-	// DECLARE HTML ELEMENTS FOR PROJECT GRID
-	var HTMLitem = '<div class="item frontend"></div>';
-	var HTMLitemImage = '<img src="%data%" class="image">';
-	var HTMLitemAnchor = '<a class="overlay" href="#" target="_blank"</a>';
-	var HTMLitemDescription = '<div class="description"></div>';
-	var HTMLitemTitle = '<h3 class="title">%data%</h3>';
-	var HTMLitemOverview = '<div class="overview"><p>%data%</p></div>';
+  // DECLARE HTML ELEMENTS FOR PROJECT GRID
+  var HTMLitem = '<li class="item '+'%data%"></li>';
+  var HTMLitemImage = '<img src="%data%" class="image">';
+  var HTMLitemAnchor = '<a class="overlay" href="#" target="_blank"</a>';
+  var HTMLitemDescription = '<div class="description"></div>';
+  var HTMLitemTitle = '<h3 class="title">%data%</h3>';
+  var HTMLitemOverview = '<div class="overview"><p>%data%</p></div>';
 
-	// APPEND HTML ELEMENTS TO PROJECT GRID
-	projects.display = function() {
-		for (project in projects.projects) {
-			var formattedItemImage = HTMLitemImage.replace("%data%", projects.projects[project].image);
-			var formattedItemDescription = HTMLitemDescription;
-			var formattedItemAnchor = HTMLitemAnchor.replace("%data%", projects.projects[project].url);
-			var formattedTitle = HTMLitemTitle.replace("%data%", projects.projects[project].name);
-			var formattedOverview = HTMLitemOverview.replace("%data%", projects.projects[project].overview);
-			$("#container").append(HTMLitem); // Append project item to grid
-			$(".item").append(formattedItemImage); // Append image to item
-			$(".item").append(formattedItemDescription); // Append description div to item
-			$(".description").append(formattedTitle).append(formattedOverview); 
-			// $(".title").append(formattedItemAnchor); // Append url to title
-		}
-	}
+  // APPEND HTML ELEMENTS TO PROJECT GRID
+  projects.display = function() {
+    for (project in projects.projects) {
+      var formattedItem = HTMLitem.replace("%data%", projects.projects[project].workType);
+      var formattedItemImage = HTMLitemImage.replace("%data%", projects.projects[project].image);
+      var formattedItemDescription = HTMLitemDescription;
+      var formattedItemAnchor = HTMLitemAnchor.replace("%data%", projects.projects[project].url);
+      var formattedItemTitle = HTMLitemTitle.replace("%data%", projects.projects[project].name);
+      var formattedItemOverview = HTMLitemOverview.replace("%data%", projects.projects[project].overview);
+      $("#project-list").append(formattedItem); // Append project item to grid
+      $(".item").append(formattedItemImage); // Append image to item
+      $(".item").append(formattedItemAnchor); // Append overlay to image
+      $(".overlay").append(formattedItemDescription); // Append description overlay
+      $(".title").append(formattedItemTitle); // Append title to description
+      $(".overview").append(formattedItemOverview); // Append overview to description
+    }
+  }
 
-	// UNCOMMENT THE BELOW FUNCTION CALL TO RUN JSON OBJECTS
-	// projects.display(); 
+  // UNCOMMENT THE BELOW FUNCTION CALL TO RUN JSON OBJECTS
+  projects.display(); 
 
-	// NAVBAR FILTER 
-	$('nav a').on('click', function() {
+  // NAVBAR FILTER 
+  $('nav a').on('click', function() {
 
-		var filter = $(this).text().toLowerCase().replace(' ', '-');
-		console.log(filter);
+    var filter = $(this).text().toLowerCase().replace(' ', '-');
+    console.log(filter);
 
-		$('#container .item').each(function() {
-			if (!$(this).hasClass(filter)) {
-				$(this).hide().addClass('.hide').removeClass('.show');
-			}
+    $('#container .item').each(function() {
+      if (!$(this).hasClass(filter)) {
+        $(this).hide().addClass('.hide').removeClass('.show');
+      }
 
-			else if ($(this).hasClass(filter)) {
-				$(this).show().addClass('.show');
-			}
+      else if ($(this).hasClass(filter)) {
+        $(this).show().addClass('.show');
+      }
 
-			else {
-				$(this).removeClass('.hide').addClass('.show');
-			}
-		});
+      else {
+        $(this).removeClass('.hide').addClass('.show');
+      }
+    });
 
-		return false;
+    return false;
 
-	});
+  });
 
-	// Clear all filter classes when ALL button is clicked
-	$('.all').on('click', function() {
-		$('div').removeClass('.hide').addClass('.show');
-	});
+  // Clear all filter classes when ALL button is clicked
+  $('.all').on('click', function() {
+    $('div').removeClass('.hide').addClass('.show');
+  });
 
-	// MODAL WINDOW FUNCTIONALITY //
-	$('.item').on('click', function popup() {
-		$('#popup').fadeIn(450);
-	});
+  // MODAL WINDOW FUNCTIONALITY //
+  $('.item').on('click', function popup() {
+    $('#popup').fadeIn(450);
+  });
 
-	var appendModal =  ("<div class='modal-overlay js-modal-close'></div>");
+  var appendModal =  ("<div class='modal-overlay js-modal-close'></div>");
 
-	  $('a[data-modal-id]').click(function(e) {
-	    e.preventDefault();
-	    $("body").append(appendModal);
-	    $(".modal-overlay").fadeTo(500, 0.7);
-	    var modalBox = $(this).attr('data-modal-id');
-	    $('#'+modalBox).fadeIn($(this).data());
-	  });  
-	  
-	$(".js-modal-close, .modal-overlay").click(function() {
-	  $(".modal-box, .modal-overlay").fadeOut(500, function() {
-	    $(".modal-overlay").remove();
-	  });
-	});
-	 
-	$(window).resize(function() {
-	  $(".modal-box").css({
-	    top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
-	    left: ($(window).width() - $(".modal-box").outerWidth()) / 2
-	  });
-	});
-	 
-	$(window).resize();
+    $('a[data-modal-id]').click(function(e) {
+      e.preventDefault();
+      $("body").append(appendModal);
+      $(".modal-overlay").fadeTo(500, 0.7);
+      var modalBox = $(this).attr('data-modal-id');
+      $('#'+modalBox).fadeIn($(this).data());
+    });  
+    
+  $(".js-modal-close, .modal-overlay").click(function() {
+    $(".modal-box, .modal-overlay").fadeOut(500, function() {
+      $(".modal-overlay").remove();
+    });
+  });
+   
+  $(window).resize(function() {
+    $(".modal-box").css({
+      top: ($(window).height() - $(".modal-box").outerHeight()) / 2,
+      left: ($(window).width() - $(".modal-box").outerWidth()) / 2
+    });
+  });
+   
+  $(window).resize();
 
 });
--- js/projectData.js
+++ js/projectData.js
@@ -3,31 +3,37 @@ var projects = {
 			"name": "Street Fighter",
 			"overview": "Street Fighter Ryu with basic jQuery effects",
 			"image": "img/projects/streetfighter.jpg",
+      "workType": "front-end mobile",
 			"url": "http://www.bumstain.github.io/jquery-streetfighter"
 		}, {
 			"name": "Tic Tac Toe",
 			"overview": "JavaScript TicTacToe",
 			"image": "img/projects/tictactoe.png",
+      "workType": "front-end mobile",
 			"url": "http://www.bumstain.github.io/javascript-tictactoe"
 		}, {
 			"name": "SearchVidz YouTube API",
 			"overview": "Video search site using the YouTube API",
 			"image": "img/projects/youtube.jpg",
+      "workType": "front-end projects",
 			"url": "http://www.bumstain.github.io/javascript-tictactoe"
 		}, {
-			"name": "Marvel Quiz Project",
-			"overview": "Basic JavaScript quiz app based on weird facts from the Marvel universe",
-			"image": "img/projects/marvelquiz.png",
-			"url": "http://www.bumstain.github.io/marvel-quiz"
-		}, {
-			"name": "Content Slider",
-			"overview": "Simple jQuery content slider",
-			"image": "img/projects/contentslider.png",
-			"url": "http://www.bumstain.github.io/content-slider"
-		}, {
+		// 	"name": "Marvel Quiz Project",
+		// 	"overview": "Basic JavaScript quiz app based on weird facts from the Marvel universe",
+		// 	"image": "img/projects/marvelquiz.png",
+    // "workType": "front-end projects",
+		// 	"url": "http://www.bumstain.github.io/marvel-quiz"
+		// }, {
+		// 	"name": "Content Slider",
+		// 	"overview": "Simple jQuery content slider",
+		// 	"image": "img/projects/contentslider.png",
+    // "workType": "front-end mobile",
+		// 	"url": "http://www.bumstain.github.io/content-slider"
+		// }, {
 			"name": "FAQ Slider",
 			"overview": "Accordion slider that uses jQuery, similar to FAQ menus",
 			"image": "img/projects/FAQslider.png",
+      "workType": "front-end",
 			"url": "http://www.bumstain.github.io/FAQ-slider"
 		}
 	]
